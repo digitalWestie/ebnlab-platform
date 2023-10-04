14d53fbe2829e3ff9635cfe6803f769766cf4c56
R Gianni <hello@rgianni.cc>
2023-10-04 16:46:52 +0100

    adds theme page for ebn

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def page
+    @page_key = request.path[1..-1]
+    render "pages/#{@page_key}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
   get "/up/", to: "up#index", as: :up
   get "/up/databases", to: "up#databases", as: :up_databases
 
+  pages = %w(
+    theme
+  )
+
+  pages.each do |page|
+    get "/#{page}", to: "pages##{page}", as: "#{page.gsub('-', '_')}"
+  end
+
+
   # Sidekiq has a web dashboard which you can enable below. It's turned off by
   # default because you very likely wouldn't want this to be available to
   # everyone in production.
