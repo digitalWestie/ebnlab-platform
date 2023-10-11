68d0d5e98901199334efcb9e68d92af62399fe7e
R Gianni <hello@rgianni.cc>
2023-10-11 16:22:20 +0100

    helpful for reroling

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,22 @@
 class Assignment < ApplicationRecord
+
+  ROLES = ["collaborator", "admin"].freeze
+
   belongs_to :user
   belongs_to :assignable, polymorphic: true
 
   validates :user, uniqueness: { scope: [:assignable_type, :assignable_id], message: "is already assigned" }
+
+  ROLES.each do |role|
+    define_method("is_#{role}?") do
+      self.role == role
+    end
+  end
+
+  def next_role
+    role_idx = Assignment::ROLES.index(self.role)
+    role_idx += 1
+    role_idx = role_idx % Assignment::ROLES.size
+    Assignment::ROLES[role_idx]
+  end
 end
