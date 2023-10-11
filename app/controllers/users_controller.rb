bcb9ff1ec4bc21de51cc54e14c854f45651c2c0e
R Gianni <hello@rgianni.cc>
2023-10-11 16:21:57 +0100

    user should only see relevant users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: []
 
   def search
+    relevant_users = User.joins(:organisations).where('organisations.id' => current_user.organisations)
     search_term = params[:email]
-    results = User.where("email LIKE ?", "%#{search_term}%").pluck(:email, :id)
+    results = relevant_users.where("email LIKE ?", "%#{search_term}%").pluck(:email, :id)
     render json: results.collect { |arr| h = { email: arr[0], id: arr[1] } }
   end
 
